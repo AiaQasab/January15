2d473a52b2b8ce719202ce624bba017959805b6d
Student <Student@VMMB310-77>
2018-01-15 11:20:43 +0200

    Added some code in VS

--- VSGitDemoA/VSGitDemoA/Program.cs
+++ VSGitDemoA/VSGitDemoA/Program.cs
@@ -11,8 +11,10 @@ namespace VSGitDemoA
         static void Main(string[] args)
         {
             //Code added by Team Member A
-            
+
             //Code added by Team Member B
+
+            //Code added by Timo in VS
         }
     }
 }
